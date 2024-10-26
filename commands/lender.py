76945b8d05d38ca4dbf1861acc33759a5e2443b6
ML <michelleliu.ca@gmail.com>
2024-10-26 16:26:45 -0700

    list lend and list borrow

--- commands/lender.py
+++ commands/lender.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+from services import external_lending_requests, external_user_requests
 
 class Lender(commands.Cog):
     def __init__(self, bot):
@@ -10,24 +11,28 @@ class Lender(commands.Cog):
     # keep track of user and give menu for card search results
     # then let user pick the card they want with scroll buttons then click confirm button to lend
     @commands.command()
-    async def lendCard(self, interaction: discord.Interaction, borrower: discord.Member, ):
+    async def lendCard(self, ctx, borrower: discord.Member, ):
         pass
     # use regular text command with params user to return card to and card to search
     # keep track of user and give menu for card search results
     # then let user pick the card they want with scroll buttons then click confirm button to return
     @commands.command()
-    async def returnCard(self, interaction: discord.Interaction, borrower: discord.Member, ):
+    async def returnCard(self, ctx, borrower: discord.Member, ):
         pass
 
     # use regular text command with params user to check lending list
     # default current user if none provided
     @commands.command()
-    async def listLentCards(self, interaction: discord.Interaction, borrower: discord.Member):
-        pass
+    async def listLentCards(self, ctx, lender: discord.Member):
+        userID = external_user_requests.getUserIdFromAPI(lender.id)
+        external_lending_requests.getListCardsLentByOwner(userID)
+        return
 
     # use regular text command with params user to check borrowing list
     # default current user if none provided
     @commands.command()
-    async def listBorrowedCards(self, interaction: discord.Interaction, borrower: discord.Member):
-        pass
+    async def listBorrowedCards(self, ctx, borrower: discord.Member):
+        userID = external_user_requests.getUserIdFromAPI(borrower.id)
+        external_lending_requests.getListCardsBorrowedByUser(userID)
+        return
 
--- example_bot.py
+++ example_bot.py
@@ -6,32 +6,32 @@ from discord.ext.commands import Bot
 
 
 from services import external_lending_requests, external_user_requests
-from commands import Lender
+from commands.lender import Lender
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = Bot(intents=intents)
+bot = Bot(command_prefix='$', intents=intents)
 
 @bot.event
 async def on_ready():
+    await bot.add_cog(Lender(bot))
     print(f'We have logged in as {bot.user}')
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+# @bot.event
+# async def on_message(message):
+#     if message.author == bot.user:
+#         return
+#
+#     if message.content.startswith('$hello'):
+#         userID = external_user_requests.getUserIdFromAPI(message.author.id)
+#         print("-----------------------------------------------------------")
+#         external_lending_requests.postLendCardFromOwnerToUser(userID, "cb418706-6703-4115-b174-0758b97e6f57", "0000419b-0bba-4488-8f7a-6194544ce91e", 4)
+#         print("-----------------------------------------------------------")
+#         external_lending_requests.getListCardsBorrowedByUser("cb418706-6703-4115-b174-0758b97e6f57")
+#         print("-----------------------------------------------------------")
+#         external_lending_requests.getListCardsLentByOwner(userID)
+#         print("-----------------------------------------------------------")
+#         await message.channel.send('Hello!')
 
-    if message.content.startswith('$hello'):
-        userID = external_user_requests.getUserIdFromAPI(message.author.id)
-        print("-----------------------------------------------------------")
-        external_lending_requests.postLendCardFromOwnerToUser(userID, "26ce6475-9666-44e8-bc0a-b78316688278", "0000419b-0bba-4488-8f7a-6194544ce91e", 4)
-        print("-----------------------------------------------------------")
-        external_lending_requests.getListCardsBorrowedByUser("26ce6475-9666-44e8-bc0a-b78316688278")
-        print("-----------------------------------------------------------")
-        external_lending_requests.getListCardsLentByOwner(userID)
-        print("-----------------------------------------------------------")
-        await message.channel.send('Hello!')
-
-bot.add_cog(Lender(bot))
 bot.run('', log_level=logging.DEBUG)
